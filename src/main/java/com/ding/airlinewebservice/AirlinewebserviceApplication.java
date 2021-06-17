fc44cbecdb8dbda0859e5da3003aef6be6d78828
Dinaker Deshini <deshinidinaker@gmail.com>
2021-06-17 12:34:27 -0400

    Initial commit

---
+++ src/main/java/com/ding/airlinewebservice/AirlinewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ding.airlinewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinewebserviceApplication.class, args);
+	}
+
+}
